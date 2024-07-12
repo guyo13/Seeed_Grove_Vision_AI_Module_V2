c59cdd6899b879a149c8766d818b36ad9634f81a
Guy Or <guydude1@gmail.com>
2024-07-12 14:03:58 +0300

    wip start script

--- start_dev_container.sh
+++ start_dev_container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-podman run -it --rm --name groveai-dev -v /home/guy/development/groveai/Seeed_Grove_Vision_AI_Module_V2:/root/groveai-code --device=/dev/ttyACM1 localhost/grove-ai:latest
+podman run -it --rm --name groveai-dev -v `pwd`:/root/groveai-code --device=/dev/ttyACM1 localhost/grove-ai:latest
